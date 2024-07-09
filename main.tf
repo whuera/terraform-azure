94a8303635345a1064e88a82da9e48db652ae9a0
whuera <whuera@gmail.com>
2024-07-09 15:22:51 -0400

    update main

--- main.tf
+++ main.tf
@@ -5,14 +5,15 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "grupo_recursos" {
   name     = "rg_terraformg1"
-  location = "West Europe"
+  location = "East US"
 }
 
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "app_plang1"
   resource_group_name = azurerm_resource_group.grupo_recursos.name
   location            = azurerm_resource_group.grupo_recursos.location
-  sku_name            = "P1v2"
+  #sku_name            = "P1v2"
+  sku_name            = "S1"
   os_type             = "Windows"
 }
 
