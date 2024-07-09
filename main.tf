baa8155a661bd88ec5d2e2be8d8a94c46a628020
whuera <whuera@gmail.com>
2024-07-08 22:43:49 -0400

    update files

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "grupo_recursos" {
   name     = "rg_terraformg1"
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "grupo_recursos" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "app_plang1"
+  resource_group_name = azurerm_resource_group.grupo_recursos.name
+  location            = azurerm_resource_group.grupo_recursos.location
   sku_name            = "P1v2"
   os_type             = "Windows"
 }
 
-resource "azurerm_windows_web_app" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+resource "azurerm_windows_web_app" "webapp" {
+  name                = "webapp-wh"
+  resource_group_name = azurerm_resource_group.grupo_recursos.name
+  location            = azurerm_service_plan.appserviceplan.location
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
 
   site_config {}
 }
