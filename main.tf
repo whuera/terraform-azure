9c56d1547bffd3d191fb3df71064ec24263c1d02
whuera <whuera@gmail.com>
2024-07-10 20:28:58 -0400

    update main

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
 # main
+# This Terraform configuration file sets up the Azure provider.
+# It configures the "azurerm" provider and enables all features.
 provider "azurerm" {
   features {}
 }
 
+/**
+ * Resource Group
+ *
+ * This resource block defines an Azure Resource Group.
+ * It creates a resource group with the specified name and location.
+ *
+ * @resource {azurerm_resource_group} grupo_recursos
+ * @param {string} name - The name of the resource group.
+ * @param {string} location - The location where the resource group will be created.
+ */
 resource "azurerm_resource_group" "grupo_recursos" {
   name     = "rg_terraformg1"
   location = "East US"
@@ -13,8 +25,8 @@ resource "azurerm_service_plan" "appserviceplan" {
   resource_group_name = azurerm_resource_group.grupo_recursos.name
   location            = azurerm_resource_group.grupo_recursos.location
   #sku_name            = "P1v2"
-  sku_name            = "S1"
-  os_type             = "Windows"
+  sku_name = "S1"
+  os_type  = "Windows"
 }
 
 resource "azurerm_windows_web_app" "webapp" {
